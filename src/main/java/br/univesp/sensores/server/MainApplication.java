8c5d6f8615359ef43214178b9dcefdc2a0bc77e0
lucasdclopes <lucas.dc.lopes@gmail.com>
2024-10-17 19:05:09 -0300

    classe de inicialização

---
+++ src/main/java/br/univesp/sensores/server/MainApplication.java
@@ -0,0 +1,9 @@
+package br.univesp.sensores.server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MainApplication extends Application {
+
+}
